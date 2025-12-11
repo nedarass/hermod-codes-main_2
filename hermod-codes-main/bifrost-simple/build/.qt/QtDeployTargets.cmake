704863bf84715093f1382bf7cfbd615819c66f95
NecmiAras <148572727+NecmiAras@users.noreply.github.com>
2025-12-11 13:58:03 +0300

    Initial commit

---
+++ hermod-codes-main/bifrost-simple/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_bifrost_FILE C:/Users/Taha/Downloads/hermod-codes-main (1)/hermod-codes-main/bifrost-simple/build/bifrost.exe)
+set(__QT_DEPLOY_TARGET_bifrost_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_bifrost_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
